9bc6f489ad3e1aab9f0557004f6dc8c0de996b76
multun <victor.collod@epita.fr>
2016-10-04 19:00:28 +0200

    bitmap: img: factorise more

--- bitmap/img.h
+++ bitmap/img.h
@@ -28,11 +28,12 @@ s_color_pix
 
 typedef unsigned char t_bw_pix;
 
+#define DEFAULT_IMG_TYPES_APPLY(F, SEP)			\
+  F(t_color_pix, color_img) SEP				\
+  F(t_bw_pix, bw_img) SEP				\
+  F(double, double_mat) SEP
 
-IMAGE_DECLARE(t_color_pix, color_img)
-IMAGE_DECLARE(t_bw_pix, bw_img)
-IMAGE_DECLARE(double, double_mat)
-
+DEFAULT_IMG_TYPES_APPLY(IMAGE_DECLARE,)
 
 #define AT(img, x, y) \
   (img->pixels[y * img->width + x])
@@ -40,34 +41,30 @@ IMAGE_DECLARE(double, double_mat)
 #define SUB_AT(img, x, y)				\
   (img->father->pixels[(y + img->yoff) * img->father->width + x + img->xoff])
 
-#define DEFAULT_IMG_TYPES_APPLY(F, SEP)			\
-  F(color_img) SEP					\
-  F(bw_img) SEP						\
-  F(double_mat) SEP
 
 // IMAGE DECLARATION //////////////////////
-#define ALLOC_IMAGE_DECLARE(TYPE)					\
+#define ALLOC_IMAGE_DECLARE(PIX_TYPE, TYPE)				\
   t_ ## TYPE * alloc_ ## TYPE (unsigned int width, unsigned int height)
-#define ALLOC_IMAGE_DEFINE(TYPE)					\
-  ALLOC_IMAGE_DECLARE(TYPE)						\
+#define ALLOC_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  ALLOC_IMAGE_DECLARE(PIX_TYPE, TYPE)					\
   {									\
     t_ ## TYPE *ret	= malloc(					\
-      sizeof(t_ ## TYPE ) + sizeof(ret->pixels[0]) * width * height);	\
+      sizeof(t_ ## TYPE ) + sizeof(PIX_TYPE) * width * height);		\
     ret->width	= width;						\
     ret->height	= height;						\
     return ret;								\
   }
 
 // SUB IMAGE DECLARATION //////////////////////
-#define ALLOC_SUB_IMAGE_DECLARE(TYPE)					\
+#define ALLOC_SUB_IMAGE_DECLARE(PIX_TYPE, TYPE)				\
   t_sub_ ## TYPE * alloc_sub_ ## TYPE (					\
     t_ ## TYPE * father,						\
     unsigned int xoff,							\
     unsigned int yoff,							\
     unsigned int width,							\
     unsigned int height)
-#define ALLOC_SUB_IMAGE_DEFINE(TYPE)					\
-  ALLOC_SUB_IMAGE_DECLARE(TYPE)						\
+#define ALLOC_SUB_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  ALLOC_SUB_IMAGE_DECLARE(PIX_TYPE, TYPE)				\
   {									\
     t_sub_ ## TYPE *ret	= malloc(sizeof(t_sub_ ## TYPE ));		\
     ret->father = father;						\
@@ -79,38 +76,38 @@ IMAGE_DECLARE(double, double_mat)
   }
 
 // IMAGE FREEING //////////////////////
-#define FREE_IMAGE_DECLARE(TYPE)		\
+#define FREE_IMAGE_DECLARE(PIX_TYPE, TYPE)	\
   void free_ ## TYPE (t_ ## TYPE *img)
-#define FREE_IMAGE_DEFINE(TYPE)						\
-  FREE_IMAGE_DECLARE(TYPE)						\
+#define FREE_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  FREE_IMAGE_DECLARE(PIX_TYPE, TYPE)					\
   {									\
     free(img);								\
   }
 
 // SUB IMAGE FREEING //////////////////////
-#define FREE_SUB_IMAGE_DECLARE(TYPE)		\
+#define FREE_SUB_IMAGE_DECLARE(PIX_TYPE, TYPE)	\
   void free_sub_ ## TYPE (t_sub_ ## TYPE *img)
-#define FREE_SUB_IMAGE_DEFINE(TYPE)					\
-  FREE_SUB_IMAGE_DECLARE(TYPE)						\
+#define FREE_SUB_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  FREE_SUB_IMAGE_DECLARE(PIX_TYPE, TYPE)				\
   {									\
     free(img);								\
   }
 
 // IMAGE MIRRORING //////////////////////
-#define ALLOC_IMAGE_TWIN_DECLARE(TYPE)			\
+#define ALLOC_IMAGE_TWIN_DECLARE(PIX_TYPE, TYPE)	\
   t_ ## TYPE * alloc_ ## TYPE ## _twin(t_ ## TYPE *img)
-#define ALLOC_IMAGE_TWIN_DEFINE(TYPE)					\
-  ALLOC_IMAGE_TWIN_DECLARE(TYPE)					\
+#define ALLOC_IMAGE_TWIN_DEFINE(PIX_TYPE, TYPE)				\
+  ALLOC_IMAGE_TWIN_DECLARE(PIX_TYPE, TYPE)				\
   {									\
     return alloc_ ## TYPE (img->width, img->height);			\
   }
 
-#define DEFINE_IMG_TOOLS(TYPE)			\
-  ALLOC_IMAGE_DEFINE(TYPE)			\
-  ALLOC_SUB_IMAGE_DEFINE(TYPE)			\
-  FREE_IMAGE_DEFINE(TYPE)			\
-  FREE_SUB_IMAGE_DEFINE(TYPE)			\
-  ALLOC_IMAGE_TWIN_DEFINE(TYPE)			\
+#define DEFINE_IMG_TOOLS(PIX_TYPE, TYPE)			\
+  ALLOC_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  ALLOC_SUB_IMAGE_DEFINE(PIX_TYPE, TYPE)			\
+  FREE_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  FREE_SUB_IMAGE_DEFINE(PIX_TYPE, TYPE)				\
+  ALLOC_IMAGE_TWIN_DEFINE(PIX_TYPE, TYPE)			\
 
 DEFAULT_IMG_TYPES_APPLY(ALLOC_IMAGE_DECLARE, ;)
 DEFAULT_IMG_TYPES_APPLY(ALLOC_SUB_IMAGE_DECLARE, ;)
