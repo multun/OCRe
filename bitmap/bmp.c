225f70530c842d14a2f86e9a0ba53606c3369ce0
multun <victor.collod@epita.fr>
2016-10-11 09:49:38 +0200

    bmp: check magic number

--- bitmap/bmp.c
+++ bitmap/bmp.c
@@ -4,17 +4,19 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "img.h"
 #include "bmp.h"
+#include "../tdefs.h"
 #include "../error.h"
 
 //UNCHECKED
-static long unsigned int getsize(int fd)
+static luint getsize(int fd)
 {
   struct stat infos;
   fstat(fd, &infos); //UNCHECKED
-  return (long unsigned int)infos.st_size;
+  return (luint)infos.st_size;
 }
 
 t_color_pix bgr_to_rgb(t_bmp_pix oldpix)
@@ -27,31 +29,35 @@ t_color_pix bgr_to_rgb(t_bmp_pix oldpix)
   return npix;
 }
 
-t_color_img *parse_bmp(char *buf, long unsigned int fsize)
+t_color_img *parse_bmp(char *buf, luint fsize)
 {
   t_bmp_file_header *fheader = (t_bmp_file_header*)buf;
   t_bmp_img_header  *iheader = (t_bmp_img_header*)(buf + sizeof(t_bmp_file_header));
 
+  const char sig[] = "BM";
+  if (fsize < 3 || memcmp(fheader->sig_BM, sig, 2) != 0)
+    FAIL0("not a BMP file");
+
   if ((sizeof(t_bmp_file_header) + sizeof(t_bmp_img_header)) >= fsize)
     FAIL0("corrupted file");
 
-  unsigned int width  = iheader->width;
-  unsigned int height = iheader->height;
-  long unsigned int size   = sizeof(t_color_pix) * width * height;
+  uint width  = iheader->width;
+  uint height = iheader->height;
+  luint size   = sizeof(t_color_pix) * width * height;
 
   t_color_img *ret	= malloc(sizeof(t_color_img) + size); // UNCHECKED
   ret->width	= width;
   ret->height	= height;
 
-  long unsigned int dest_offset = sizeof(t_color_pix) * width * (height - 1);
+  luint dest_offset = sizeof(t_color_pix) * width * (height - 1);
   t_color_pix *dest_cur = (t_color_pix*)((char*)ret->pixels + dest_offset);
   t_bmp_pix *orig_cur = (t_bmp_pix*)(buf + fheader->imageDataOffset);
 
-  unsigned int offset = 4 - ((ret->width * 3) & 3);
+  uint offset = 4 - ((ret->width * 3) & 3);
 
-  for(unsigned int line = 0; line < height; line++)
+  for(uint line = 0; line < height; line++)
   {
-    for(unsigned int col = 0; col < width; col++)
+    for(uint col = 0; col < width; col++)
       *dest_cur++ = bgr_to_rgb(*orig_cur++);
     orig_cur = (t_bmp_pix*)((char*)orig_cur + offset);
     dest_cur = (t_color_pix*)((char*)dest_cur - 2 * width * sizeof(t_color_pix));
@@ -63,9 +69,9 @@ t_color_img *parse_bmp(char *buf, long unsigned int fsize)
 //UNCHECKED
 t_color_img *load_bmp(char path[])
 {
-  int			fd	= open(path, 0, O_RDONLY);
-  long unsigned int	fsize	= getsize(fd);
-  void			*buf	= mmap(
+  int   fd	= open(path, 0, O_RDONLY);
+  luint	fsize	= getsize(fd);
+  void	*buf	= mmap(
     NULL,
     fsize,
     PROT_READ,
