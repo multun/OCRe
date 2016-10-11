cb269fa80c8e937edffad36948c163bb62833d63
multun <victor.collod@epita.fr>
2016-10-12 01:59:33 +0200

    gtk: display simple image

--- gtk/pixbuf.c
+++ gtk/pixbuf.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include "../bitmap/img.h"
+#include "../tdefs.h"
 
 void pixbuf_color_img_destroy(guchar *pixels, gpointer img)
 {
@@ -7,15 +8,30 @@ void pixbuf_color_img_destroy(guchar *pixels, gpointer img)
   free_color_img((t_color_img*)img);
 }
 
-GdkPixbuf *pixbuf_from_img(t_color_img *img)
+void refresh_pixbuf_from_img(GdkPixbuf *gimg, t_color_img *img)
 {
-  return gdk_pixbuf_new_from_data ((guchar*)img->pixels,
-				   GDK_COLORSPACE_RGB,
-				   FALSE,
-				   8,
-				   (int)img->width,
-				   (int)img->height,
-				   (int)img->width,
-				   pixbuf_color_img_destroy,
-				   (gpointer)img);
+  gpointer row = gdk_pixbuf_get_pixels(gimg);
+  int rowstride = gdk_pixbuf_get_rowstride(gimg);
+
+  for(uint y = 0; y < img->height; y++)
+  {
+    guchar *cur = (guchar*)row;
+    for(uint x = 0; x < img->width; x++)
+    {
+      t_color_pix pix = AT(img, x, y);
+      *(cur++) = pix.r;
+      *(cur++) = pix.g;
+      *(cur++) = pix.b;
+    }
+    row = (gpointer)((guchar*)row + rowstride);
+  }
+}
+
+GdkPixbuf *alloc_pixbuf_from_img(t_color_img *img)
+{
+  return gdk_pixbuf_new(GDK_COLORSPACE_RGB,
+			FALSE,
+			8,
+			(int)img->width,
+			(int)img->height);
 }
--- main.c
+++ main.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
+#include <gtk-3.0/gtk/gtk.h>
 #include "bitmap/img.h"
 #include "bitmap/bmp.h"
 #include "binarisation/binarise.h"
 #include "memtools.h"
 #include "error.h"
 #include "filtering.h"
+#include "gtk/pixbuf.h"
+
+
+// called when window is closed
+void on_window_main_destroy(void)
+{
+  puts("main_quit");
+  gtk_main_quit();
+}
+
 
 int main(int argc, char *argv[])
 {
   if (argc < 2)
-    FAIL("use: %s image", argv[0]);
+    FAIL("use: %s image", argv[(argc--) - 1]);
   t_color_img *img = load_bmp(argv[1]);
 
   t_bw_img *bw_img = binarise(SAUVOLA, img);
   t_sub_bw_img *sub = alloc_sub_bw_img(bw_img, 12, 1, 42, 42);
 
-  for(unsigned int y = 0; y < img->height; y++)
-  {
-    for(unsigned int x = 0; x < img->width; x++)
-      printf((bw_img->pixels[y*img->width + x]) ? "#" : ".");
-    printf("\n");
-  }
+  // GTK PART
+
+  GtkBuilder      *builder;
+  GtkWidget       *window;
+
+  gtk_init(&argc, &argv);
+
+  builder = gtk_builder_new();
+  gtk_builder_add_from_file (builder, "window_main.glade", NULL);
+
+  window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
+  //gtk_builder_connect_signals(builder, NULL);
+
+  g_signal_connect (G_OBJECT(window), "destroy", (GCallback)on_window_main_destroy, NULL);
+
+
+  GtkImage *gtk_img = (GtkImage*)GTK_WIDGET(gtk_builder_get_object(builder, "image"));
+  GdkPixbuf *pixbuf = alloc_pixbuf_from_img(img);
+  refresh_pixbuf_from_img(pixbuf, img);
+  gtk_image_set_from_pixbuf(gtk_img, pixbuf);
+
+  gtk_widget_show(window);
+  gtk_main();
+
+  g_object_unref(builder);
+  g_object_unref(pixbuf);
+
+  // GTK_END
+
   free_bw_img(bw_img);
   free_sub_bw_img(sub);
   free_color_img(img);
