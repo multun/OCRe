ae5d2a478802d990d5fd6300119ec3e6253b771f
Louis Tamagny <louis.tamagny@epita.fr>
2016-10-19 15:20:34 +0200

    i

--- gtk/bootstrap.c
+++ gtk/bootstrap.c
@@ -8,9 +8,10 @@ void on_window_main_destroy(void)
 
 GtkWidget *gtk_bootstrap(GtkBuilder **builder)
 {
-  *builder = gtk_builder_new();
-  gtk_builder_add_from_file(*builder, "window_main.glade", NULL);
-  GtkWidget *window = _GET_WIDGET(*builder, "window_main");
-  _GTK_CONNECT(window, "destroy", on_window_main_destroy, NULL);
-  return window;
+ *builder = gtk_builder_new();
+ gtk_builder_add_from_file(*builder, "window_main.glade", NULL);
+ GtkWidget *window = _GET_WIDGET(*builder, "window_main");
+ printf("builder: %p\nwindow: %p\n", (void*)*builder, (void*)window);
+ _GTK_CONNECT(window, "destroy", on_window_main_destroy, NULL);
+ return window;
 }
