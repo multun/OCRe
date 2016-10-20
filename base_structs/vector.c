198a56589efb02eed103330ffd24ef41366493bb
multun <victor.collod@epita.fr>
2016-10-20 18:20:11 +0200

    base_structs: fix broken vector type

--- base_structs/vector.c
+++ base_structs/vector.c
@@ -2,13 +2,15 @@
 #include <string.h>
 #include "vector.h"
 
-
-//LOLNOP
+#ifdef VECTOR_DEBUG
 #include <stdio.h>
+#endif
 int generic_vector_resize(size_t *size, size_t nsize, void **data)
 {
   void *ndata;
+#ifdef VECTOR_DEBUG
   printf("resizing from %lu to %lu\n", *size, nsize);
+#endif
   if ((ndata = realloc(*data, nsize)) == NULL)
   {
     void *former = *data;
@@ -30,6 +32,9 @@ DECL_VECTOR(void);
 
 void	*generic_vector_alloc(size_t size)
 {
+#ifdef VECTOR_DEBUG
+  printf("allocating %lu\n", size);
+#endif
   t_void_vect *ret;
   if ((ret = malloc(sizeof(t_void_vect))) == NULL
       || ((ret->data.gen = malloc(size)) == NULL))
--- base_structs/vector.h
+++ base_structs/vector.h
@@ -22,6 +22,9 @@
 int	generic_vector_resize(size_t *size, size_t nsize, void **data);
 void	*generic_vector_alloc(size_t size);
 
+#define VECT_PRINT(VECT)					\
+  printf("vect(i:%lu size: %lu)\n", VECT->i, VECT->size);
+
 #define VECT_RESIZE_ABS(VECT, SIZE)					\
   ((SIZE > VECT->size)							\
    ? generic_vector_resize(&(VECT->size), SIZE, &(VECT->data.gen))	\
@@ -29,19 +32,20 @@ void	*generic_vector_alloc(size_t size);
 #define VECT_EXPAND(VECT)						\
   VECT_RESIZE_ABS(VECT, (VECT->size) * 2)
 #define VECT_PUSH(VECT, VAL)					\
-  (((++(VECT->i) < VECT->size) ? (VECT_EXPAND(VECT)) : 0)	\
-  || (VECT_GET(VECT, VECT->i) = VAL, 0));
+  ((((VECT->i+1) > VECT->size) ? (VECT_EXPAND(VECT)) : 0)	\
+   || (VECT_GET(VECT, (VECT->i)++) = VAL, 0));
 
 #define VECT_RESIZE(VECT, SIZE)					\
   VECT_RESIZE_ABS(VECT, SIZE * sizeof(*(VECT->data.spec)))
 
 #define VECT_GET(VECT, INDEX) (VECT->data.spec[INDEX])
-#define VECT_GET_LAST(VECT) VECT_GET(VECT, VECT->i)
+#define VECT_GET_LAST(VECT) VECT_GET(VECT, VECT->i - 1)
 #define VECT_GETP(VECT, INDEX) (&(VECT_GET(VECT, INDEX)))
 #define VECT_ALLOC(TYPE, SIZE)						\
   (VECT_NAME(t, TYPE)*)generic_vector_alloc(				\
-    SIZE * sizeof(((VECT_NAME(t, TYPE)*)0)->data.spec))
-#define VECT_GET_SIZE(VECT) (VECT->size / sizeof(*(VECT->data.spec)))
+    SIZE * sizeof(*(((VECT_NAME(t, TYPE)*)0)->data.spec)))
+#define VECT_GET_SIZE(VECT) (VECT->i)
+#define VECT_GET_ALLOC(VECT) (VECT->size / sizeof(*(VECT->data.spec)))
 #define VECT_FREE(VECT) (free(VECT->data.gen), free(VECT))
 
 #endif
