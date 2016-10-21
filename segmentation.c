8ac16fe3f0aef227afe020bf575ba379b2682d23
OudeaBrieg <brieg.oudea@epita.fr>
2016-10-22 01:00:42 +0200

    segmentation: add imgToSmallerImg + Normalization

--- segmentation.c
+++ segmentation.c
@@ -4,7 +4,7 @@ int isWhiteColumn(t_sub_bw_img *img, int column)
     
     for(uint i = 0; i< img -> height; i++)
     {
-        if(img[i][column].color() != [255,255,255]) ##Not sure about the syntax('if this pixel of img is not white')
+        if(AT(img,i,column) == 0)
         {
             isWhite = 0;
         }
@@ -23,15 +23,27 @@ t_sub_bw_img_vect segmentation(t_sub_bw_img *img)
     {
         if (isWhiteColumn(img, i) || i == width-1)
         {
-            if(i>0)
-            {
-                if(isWhiteColumn(img, i-1) == 0)
-                {
-                    result[elements++] = img[aux->i] ##result gets appended img(from column aux to i)
-                }
+            if(i>0 && (isWhiteColumn(img, i-1) == 0))
+            {                
+                result[elements++] = imgToSmallerImg(img, aux, i];   
             }
             aux++;
         }     
+    }
+    return result;
+}
+
+t_sub_bw_img imgToSmallerImg(t_sub_bw_img *img, int firstColumn, int lastColumn)
+{
+    t_sub_bw_img *result;
+    int aux;
+    for(int i = firstColumn; i <= lastColumn; i++)
+    {
+        aux = i - firstColumn;
+        for(int j = 0; j < img -> height; j++)
+        {
+            AT(result, j, aux) = AT(img, j, i); 
+        }        
     }
     return result;
 }
