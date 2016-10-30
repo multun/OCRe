5a980d9329b2125342228e46e709fc067b54c76f
Nathan Tournant <nathan.tournant@epita.fr>
2016-10-30 20:15:58 +0100

    block_to_line hotfix

--- block_to_line/block_to_line.c
+++ block_to_line/block_to_line.c
@@ -58,7 +58,7 @@ t_bool *bool_array_generation(int average, int *array, t_sub_bw_img *img)
       bool_array[i] = true;
     }
     //printf("Array[%d] = %d, bool = %u, this_sum = %d, this_average = %d\n"
-                    ,i,array[i],bool_array[i],sum_array[i],averages_array[i]);
+                    //,i,array[i],bool_array[i],sum_array[i],averages_array[i]);
   }
 //RETOUCHE 1 de bool_array
 
