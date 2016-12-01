77f2761adbd2c903fc72ffdf34016cefc39e4c48
multun <victor.collod@epita.fr>
2016-12-01 02:27:51 +0100

    character_segmentation: working

--- character_segmentation/segmentation.c
+++ character_segmentation/segmentation.c
@@ -9,17 +9,18 @@ typedef struct s_off_c
   int x, y;
 } t_off_c;
 
-static void update_shape(t_shape *shapes, uint x, uint y)
+static void update_shape(t_shape *shape, uint x, uint y)
 {
-  shapes->Xmin = MIN(shapes->Xmin, x);
-  shapes->Xmax = MAX(shapes->Xmax, x);
-  shapes->Ymin = MIN(shapes->Ymin, y);
-  shapes->Ymax = MAX(shapes->Ymax, y);
+  shape->size++;
+  shape->Xmin = MIN(shape->Xmin, x);
+  shape->Xmax = MAX(shape->Xmax, x);
+  shape->Ymin = MIN(shape->Ymin, y);
+  shape->Ymax = MAX(shape->Ymax, y);
 }
 
 static void taint(t_shape *shape, t_int_mat *mat, uint x, uint y, int label)
 {
-  t_off_c neighbours[] = {
+  t_off_c const neighbours[] = {
     {-1, -1},
     {-1, 0},
     {-1, 1},
@@ -32,11 +33,14 @@ static void taint(t_shape *shape, t_int_mat *mat, uint x, uint y, int label)
 
   switch(AT(mat, x, y))
   {
-    case 0:
-    case -1:
-        return;
+  case 0:
+  default:
+    return;
+  case -1:
+    break;
   }
 
+  update_shape(shape, x, y);
   AT(mat, x, y) = label;
   for (uint i = 0; i < sizeof(neighbours)/sizeof(neighbours[0]); i++)
   {
@@ -44,20 +48,17 @@ static void taint(t_shape *shape, t_int_mat *mat, uint x, uint y, int label)
     npos.x += (int)x;
     npos.y += (int)y;
     if(npos.x > 0 && npos.y > 0 && \
-       npos.x < (int)mat->width && npos.y < (int)mat->height){
-      update_shape(shape, (uint)npos.x, (uint)npos.y);
+       npos.x < (int)mat->width && npos.y < (int)mat->height)
       taint(shape, mat, (uint)npos.x, (uint)npos.y, label);
-    }
   }
 }
 
 static void add_shape(t_int_mat *mat,uint x,uint y,t_shape_vect *shapes)
 {
   t_shape *ret = malloc(sizeof(t_shape));
-  ret->Xmin = x;
-  ret->Xmax = x;
-  ret->Ymin = y;
-  ret->Ymax = y;
+  ret->Xmin = ret->Xmax = x;
+  ret->Ymin = ret->Ymax = y;
+  ret->size = 1;
   VECT_PUSH(shapes, ret);
   taint(VECT_GET_LAST(shapes), mat, x, y, (int)VECT_GET_SIZE(shapes));
 }
@@ -81,14 +82,12 @@ t_sub_bw_img_vect *char_segmentation(t_sub_bw_img *img)
   for (unsigned int i = 0; i < VECT_GET_SIZE(shapes);i++)
   {
     t_shape *tempShape = VECT_GET(shapes, i);
-    tempShape->Xsize = tempShape->Xmax - tempShape->Xmin;
-    tempShape->Ysize = tempShape->Ymax - tempShape->Ymin;
     t_sub_bw_img *sub = relink_sub_bw_img(
       img,
       tempShape->Xmin,
       tempShape->Ymin,
-      tempShape->Xsize,
-      tempShape->Ysize);
+      tempShape->Xmax - tempShape->Xmin,
+      tempShape->Ymax - tempShape->Ymin);
     VECT_PUSH(result, sub);
   }
   return result;
