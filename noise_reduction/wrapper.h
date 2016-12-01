28e80770a72e3bd815287b361977e81fb9540400
Nathan Tournant <nathan.tournant@epita.fr>
2016-12-01 18:21:37 +0100

    noise_reduction: attempt to a wrapper

---
+++ noise_reduction/wrapper.h
@@ -0,0 +1,12 @@
+#ifndef NOISE_RED_H
+#define NOISE_REDUCTION_H
+
+#include<stdio.h>
+#include "../bitmap/bmp.h"
+#include "../bitmap/img.h"
+#include"noise_reduction.h"
+#include "../tdefs.h"
+
+t_bw_img* noise_red(t_bw_img *img);
+
+#endif
