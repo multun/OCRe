006abbbcaeb19254aadf5d39b48a0fcc7281fe78
multun <victor.collod@epita.fr>
2016-12-02 22:55:41 +0100

    gtk: vectors: add missing include

--- gtk/vectors.h
+++ gtk/vectors.h
@@ -1,6 +1,9 @@
 #ifndef GTK_VECTORS_H
 #define GTK_VECTORS_H
 
+#include "../base_structs/vector.h"
+#include "../bitmap/img.h"
+
 DECL_NAMED_VECTOR(t_l_color_img*, l_color_img);
 DECL_NAMED_VECTOR(t_l_bw_img*, l_bw_img);
 
