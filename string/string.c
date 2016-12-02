bba1bafcc3f38ea7651229cde75dfc260623d0f4
multun <victor.collod@epita.fr>
2016-12-02 22:52:02 +0100

    string: add free, get_c_string, alloc_sized_string

--- string/string.c
+++ string/string.c
@@ -5,12 +5,17 @@
 t_string *alloc_string(const char *c_str)
 {
   const size_t len = strlen(c_str);
-  t_string *res = VECT_ALLOC(char, len + 2);
-  for(size_t i = 0; i < len + 1; i++)
-    VECT_PUSH(res, c_str[i]);
-  return res;
+  return alloc_sized_string(c_str, len + 2);
 }
 
+t_string *alloc_sized_string(const char *c_str, size_t size)
+{
+  t_string *res = VECT_ALLOC(char, size);
+  for(size_t i = 0; c_str[i]; i++)
+    VECT_PUSH(res, c_str[i]);
+  VECT_PUSH(res, '\0');
+  return res;
+}
 
 void string_append(t_string *dst, const t_string *src)
 {
@@ -30,4 +35,14 @@ void string_c_append(t_string *dst, const char *c_str)
 size_t string_len(t_string *str)
 {
   return VECT_GET_SIZE(str) - 1;
+}
+
+void string_free(t_string *str)
+{
+  VECT_FREE(str);
+}
+
+char *string_get_c(t_string *str)
+{
+  return &VECT_GET(str, 0);
 }
--- string/string.h
+++ string/string.h
@@ -11,8 +11,11 @@ typedef t_char_vect t_string;
 
 
 t_string *alloc_string(const char *c_str);
+t_string *alloc_sized_string(const char *c_str, size_t size);
 void string_append(t_string *dst, const t_string *src);
 void string_c_append(t_string *dst, const char *src);
 size_t string_len(t_string *str);
+char *string_get_c(t_string *str);
+void string_free(t_string *str);
 
 #endif
