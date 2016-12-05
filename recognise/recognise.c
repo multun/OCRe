86257f1c17fee0ab90099086f23ff9634936bd92
multun <victor.collod@epita.fr>
2016-12-05 13:04:37 +0100

    recognise: remove printfs

--- recognise/recognise.c
+++ recognise/recognise.c
@@ -29,7 +29,6 @@ t_string *recognise_bw_img(t_w_network_vect *net, t_bw_img *img)
 static t_string *recognise_block(t_w_network_vect *net, t_block *block)
 {
   t_string *res = alloc_sized_string("", 500);
-  //t_sub_bw_img_vect *lines = line_subdivision(block);
   t_sub_bw_img_vect *lines = detect_lines(block);
   for(size_t i = 0; i < VECT_SIZE(lines); i++)
   {
@@ -53,24 +52,12 @@ static t_string *recognise_line(t_w_network_vect *net, t_line *line)
     t_char *cur = VECT_GET(chars, i);
     if(cur)
     {
-      for(uint y = 0; y < cur->height; y++)
-      {
-	for(uint x = 0; x < cur->width; x++)
-	  printf("%3d", L_AT(cur, x, y));
-	puts("");
-      }
-      puts("");
-
       t_string *char_content = char_recon(net, cur);
       string_append(res, char_content);
       string_free(char_content);
     }
     else
-    {
-      puts("=============================");
       string_c_append(res, " ");
-
-    }
     free_l_bw_img(cur);
   }
   VECT_FREE(chars);
